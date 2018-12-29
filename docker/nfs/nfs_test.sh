2152958cba3355656db5aca6bf9f4bc1e081d1ef
mosvodokanal <Mvk-ais@yandex.ru>
2018-12-29 11:33:15 +0300

    updates

--- docker/nfs/nfs_test.sh
+++ docker/nfs/nfs_test.sh
@@ -7,10 +7,11 @@ docker volume create \
     --driver local \
     --opt type=nfs \
     --opt o=addr=172.16.55.221,rw \
-    --opt device=:/mnt/share/p/Shared/html \
+    --opt device=:/mnt/share/p/Shared/db \
     mynfs
 
 docker run --name nfs-test -it -v mynfs:/foo alpine sh
 
 docker container rm nfs-test
 docker volume rm mynfs
+
